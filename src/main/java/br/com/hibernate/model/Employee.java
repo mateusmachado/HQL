fe02fa062a6d265433b219d96e961b8427fca150
Mateus <mmachado.csp@gmail.com>
2015-07-20 20:03:52 -0300

    Added Employee, Address

---
+++ src/main/java/br/com/hibernate/model/Employee.java
@@ -0,0 +1,51 @@
+package br.com.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "EMPLOYEE")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "salary")
+	private double salary;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
